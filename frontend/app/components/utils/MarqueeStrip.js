c8f6675f4e680e02b2a43fd58eee80dd2297a21e
Ayush Rawat <ayushrf@gmail.com>
2026-09-17 09:56:56 +0530

    fix: update the servies

--- frontend/app/components/utils/MarqueeStrip.js
+++ frontend/app/components/utils/MarqueeStrip.js
@@ -5,6 +5,7 @@ import React from 'react';
 export default function MarqueeStrip() {
   const items = [
     "PAINT CORRECTION",
+    "Graphene Coating",
     "ENDEAVOUR HILLS",
     "TINT REMOVAL",
     "WINDOW TINTING",
